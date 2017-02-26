b4de80637826e11e77b7666f3e9ffc07a1174253
Kevin <kgresmer@gmail.com>
2017-02-26 12:11:31 -0800

    Added the start of the implementation

--- src/main/arrays/FindMinInRotatedSortedArray.java
+++ src/main/arrays/FindMinInRotatedSortedArray.java
@@ -4,7 +4,20 @@ package main.arrays;
 public class FindMinInRotatedSortedArray {
 
     public static int findMinimum(int[] input) {
+        if (input.length == 0) {
+            return -1;
+        }
+        int lowest = input[0];
+        int current;
+        int left = input[0];
+        int right = input[input.length/2];
+        while (left < right) {
+            if (left > right) {
+                lowest = right;
+            } else {
 
-        return 3;
+            }
+        }
+        return lowest;
     }
 }
