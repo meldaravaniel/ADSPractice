1196f56ad4e3741c93b1c59ec0d32e7f4b85b296
kgresmer <Jasong55>
2017-03-06 07:34:23 -0800

    Remove duplicates without a buffer linked list

--- src/test/linkedlists/RemoveDupsFromUnsortedTest.java
+++ src/test/linkedlists/RemoveDupsFromUnsortedTest.java
@@ -22,7 +22,30 @@ public class RemoveDupsFromUnsortedTest {
                 Arrays.asList(1,3,4,5,6,7)
         );
         LinkedList result = RemoveDupsFromUnsorted.removeDups(temp1);
-        assertEquals(eresult.size(), result.size());
+        assertTrue(eresult.equals(result));
+    }
+
+    @Test
+    public void removeDupsNoBuffer() throws Exception {
+        LinkedList temp1 = new LinkedList<>(
+                Arrays.asList(1,3,4,5,6,7,3,4,5, 8,9,10)
+        );
+        LinkedList eresult = new LinkedList<>(
+                Arrays.asList(1,3,4,5,6,7,8,9,10)
+        );
+        LinkedList result = RemoveDupsFromUnsorted.removeDupsNoBuffer(temp1);
+        assertTrue(eresult.equals(result));
+    }
+
+    @Test
+    public void removeDupsNoBuffer2() throws Exception {
+        LinkedList temp1 = new LinkedList<>(
+                Arrays.asList(3,3)
+        );
+        LinkedList eresult = new LinkedList<>(
+                Arrays.asList(3)
+        );
+        LinkedList result = RemoveDupsFromUnsorted.removeDupsNoBuffer(temp1);
         assertTrue(eresult.equals(result));
     }
 
