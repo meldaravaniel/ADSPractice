69ebe00732eee2d3d65689f80c050bb7b8f0a24c
kgresmer <Jasong55>
2017-03-28 12:35:08 -0700

    Added traversal of binary trees in 3 ways.

--- src/main/binarytrees/BinaryTree.java
+++ src/main/binarytrees/BinaryTree.java
@@ -35,20 +35,61 @@ public class BinaryTree {
 
     public void inOrderTraverseTree(TreeNode focusNode) {
         if (focusNode != null) {
+            System.out.println("Focus node: " + focusNode.key);
+        } else {
+            System.out.println("Null");
+        }
+        if (focusNode != null) {
             inOrderTraverseTree(focusNode.leftChild);
             System.out.println(focusNode);
             inOrderTraverseTree(focusNode.rightChild);
         }
     }
 
+    public void preOrderTraverseTree(TreeNode focusNode) {
+        if (focusNode != null) {
+            System.out.println("Focus node: " + focusNode.key);
+        } else {
+            System.out.println("Null");
+        }
+        if (focusNode != null) {
+            System.out.println(focusNode);
+            preOrderTraverseTree(focusNode.leftChild);
+            preOrderTraverseTree(focusNode.rightChild);
+        }
+    }
+
+    public void postOrderTraverseTree(TreeNode focusNode) {
+        if (focusNode != null) {
+            System.out.println("Focus node: " + focusNode.key);
+        } else {
+            System.out.println("Null");
+        }
+        if (focusNode != null) {
+            postOrderTraverseTree(focusNode.leftChild);
+            postOrderTraverseTree(focusNode.rightChild);
+            System.out.println(focusNode);
+        }
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
+        tree.addNode(30, "irty");
         tree.addNode(20, "Boss");
-        tree.addNode(25, "twenty five");
-        tree.addNode(15, "fifteen");
         tree.addNode(166, "one hund 66");
-        tree.addNode(30, "thirty");
+        tree.addNode(25, "twenty five");
+
+        tree.addNode(40, "thiry");
+        tree.addNode(15, "fifteen");
+
+
+        tree.addNode(35, "trty");
+        tree.addNode(50, "ty");
 
         tree.inOrderTraverseTree(tree.root);
+        System.out.println("Pre");
+        tree.preOrderTraverseTree(tree.root);
+        System.out.println("Post");
+        tree.postOrderTraverseTree(tree.root);
     }
 }
