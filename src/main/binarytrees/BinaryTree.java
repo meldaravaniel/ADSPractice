f30624357cc09ac1794e2d8f6d1a6ad46d43c3b8
kgresmer <Jasong55>
2017-03-30 09:20:22 -0700

    tried to add delete node method.

--- src/main/binarytrees/BinaryTree.java
+++ src/main/binarytrees/BinaryTree.java
@@ -72,24 +72,119 @@ public class BinaryTree {
         }
     }
 
+    public TreeNode findNode(int key) {
+        TreeNode focusNode = root;
+        while(focusNode.key != key){
+            if (key < focusNode.key) {
+                focusNode = focusNode.leftChild;
+            } else {
+                focusNode = focusNode.rightChild;
+            }
+
+            if (focusNode == null) {
+                return null;
+            }
+        }
+        return focusNode;
+    }
+
+    public boolean deleteNode(int key) {
+        if (root == null) return false;
+        TreeNode focusNode = root;
+        TreeNode parent = root;
+
+        boolean isItALeftChild = true;
+
+        while (focusNode.key != key) {
+            parent = focusNode;
+            if (key < focusNode.key) {
+                isItALeftChild = true;
+                focusNode = focusNode.leftChild;
+            } else {
+                isItALeftChild = false;
+                focusNode = focusNode.rightChild;
+            }
+
+            if (focusNode == null) return false;
+        }
+
+        if (focusNode.leftChild == null && focusNode.rightChild == null) {
+            if (focusNode == root) {
+                root = null;
+            } else if (isItALeftChild) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
+            }
+        } else if (focusNode.rightChild == null) {
+            if (focusNode == root) {
+                root = focusNode.leftChild;
+            } else if (isItALeftChild) {
+                parent.leftChild = focusNode.leftChild;
+            } else {
+                parent.rightChild = focusNode.leftChild;
+            }
+        } else if (focusNode.leftChild == null) {
+            if (focusNode == root) {
+                root = focusNode.rightChild;
+            } else if (isItALeftChild) {
+                parent.leftChild = focusNode.rightChild;
+            } else {
+                parent.rightChild = focusNode.rightChild;
+            }
+        } else {
+            TreeNode replacement = getReplacementNode(focusNode);
+
+            if (focusNode == root) {
+                root = replacement;
+            } else if (isItALeftChild) {
+                parent.leftChild = replacement;
+            } else {
+                parent.rightChild = replacement;
+            }
+            replacement.leftChild = focusNode.leftChild;
+        }
+
+        return true;
+    }
+
+    private TreeNode getReplacementNode(TreeNode nodeToBeReplaced) {
+
+        TreeNode replacementParent = nodeToBeReplaced;
+        TreeNode replacement = nodeToBeReplaced;
+
+        TreeNode focusNode = nodeToBeReplaced.rightChild;
+        while(focusNode != null) {
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.leftChild;
+        }
+
+        if (replacement != nodeToBeReplaced.rightChild) {
+            replacement.leftChild = replacement.rightChild;
+            replacement.rightChild = nodeToBeReplaced.rightChild;
+        }
+        return replacement;
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.addNode(30, "irty");
         tree.addNode(20, "Boss");
         tree.addNode(166, "one hund 66");
         tree.addNode(25, "twenty five");
-
         tree.addNode(40, "thiry");
         tree.addNode(15, "fifteen");
-
-
         tree.addNode(35, "trty");
         tree.addNode(50, "ty");
 
-        tree.inOrderTraverseTree(tree.root);
-        System.out.println("Pre");
-        tree.preOrderTraverseTree(tree.root);
-        System.out.println("Post");
-        tree.postOrderTraverseTree(tree.root);
+//        tree.inOrderTraverseTree(tree.root);
+//        System.out.println("Pre");
+//        tree.preOrderTraverseTree(tree.root);
+//        System.out.println("Post");
+//        tree.postOrderTraverseTree(tree.root);
+
+        System.out.println("Find node: 35");
+        System.out.println(tree.findNode(35));
     }
 }
